a134173dfc3f664bf1d9d4ebfea6b6237b38e63a
ks <146908390+ks415@users.noreply.github.com>
2025-01-11 18:16:51 +0900

    Firebaseからデータを取得しHomeに表示

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data);
+      // console.log(data.docs);
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+    getPosts();
+  },[]);
+
   return (
     <div className='homePage'>
-      <div className='postContents'>
+      {postList.map((post) => {
+        return (
+          <div className='postContents' key={post.id}>
         <div className='postHeader'>
-          <h1>タイトル</h1>
+          <h1>{post.title}</h1>
         </div>
         <div className='posttextContainer'>
-          今はReactの学習をしています．
+          {post.postText}
         </div>
         <div className='nameAndDeleteButton'>
-          <h3>@kawauso415</h3>
+          <h3>{post.author.username}</h3>
           <button>削除</button>
         </div>
       </div>
+        )
+      })}
     </div>
   );
 }

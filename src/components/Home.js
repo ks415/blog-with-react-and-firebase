c210e79a53b5b9173be1023fa7e3cd76305fd8dc
ks <146908390+ks415@users.noreply.github.com>
2025-01-11 18:00:34 +0900

    Homeの実装

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div className='homePage'>
+      <div className='postContents'>
+        <div className='postHeader'>
+          <h1>タイトル</h1>
+        </div>
+        <div className='posttextContainer'>
+          今はReactの学習をしています．
+        </div>
+        <div className='nameAndDeleteButton'>
+          <h3>@kawauso415</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home
